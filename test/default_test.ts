c003bf35c586d53af6f35f18de80ecca5fc4ecc4
Gyusun Yeom <omniavinco@gmail.com>
2017-01-23 01:17:43 +0900

    fix test - async test

--- test/default_test.ts
+++ test/default_test.ts
@@ -3,28 +3,27 @@ import { suite, test, slow, timeout, skip, only } from "mocha-typescript";
 import { Transformer } from "../src";
 import * as stream from "stream";
 
-function promisefy(fn: (...args: any[]) => any, ...args: any[]): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-        try {
-            fn(...args, resolve);
-        } catch (e) {
-            reject(e);
-        }
-    });
+function transformTest(stream: stream.Transform, input: string, expected: string, done: () => void) {
+    let output = "";
+    stream
+    .on('data', (d: any) => output = output + d)
+    .on('finish', () => {
+        assert.equal(output, expected);
+        done();
+    })
+    .on('error', (e: Error) => {
+        assert.ifError(e);
+    })
+    .end(input);
 }
 
 @suite class Default {
-    @test textNodeChange() {
-        const htmlBuffer = new stream.Duplex();
+    @test "text node transform"(done: () => void) {
         const transformer = new Transformer();
-        let output = "";
-
-        transformer.onText(/hello, (.+)/, text => "censored");
-        htmlBuffer.pipe(transformer);
-        transformer.on('data', (d : any) => output = output + d);
-
+        console.log("begin");
         transformer.setEncoding("utf-8");
-        transformer.end(`<html>
+        transformer.onText(/hello, (.+)/, text => "censored");
+        transformTest(transformer, `<html>
 <head>
     </head>
     <body>
@@ -32,9 +31,7 @@ function promisefy(fn: (...args: any[]) => any, ...args: any[]): Promise<void> {
         <span>hello, everyone!</span>
         <span>hello nothing</span>
     </body>
-</html>`);
-        transformer.on('end', () => {
-            assert.equal(output , `<html>
+</html>`, `<html>
 <head>
     </head>
     <body>
@@ -42,71 +39,54 @@ function promisefy(fn: (...args: any[]) => any, ...args: any[]): Promise<void> {
         <span>censored</span>
         <span>hello nothing</span>
     </body>
-</html>`);
-        });
+</html>`, done);
     }
 
-    @test attributeTransformTest() {
-        const htmlBuffer = new stream.Duplex();
+    @test "Transform the attribute value of specific tag, attribute"(done: () => void) {
         const transformer = new Transformer();
         let output = "";
 
-        transformer.onTag("div", "X-nothing", /hello/, text => "censored");
-        htmlBuffer.pipe(transformer);
-        transformer.on('data', (d : any) => output = output + d);
-
-        transformer.setEncoding("utf-8");
-        transformer.end(`<html>
+        transformer.onTag("div", "x-nothing", /hello/, text => "censored");
+        transformTest(transformer, `<html>
 <head>
     </head>
     <body>
-        <div X-nothing="hello">hello, world!</div>
-        <div X-something="hello">hello, everyone!</div>
-        <span X-nothing="hello">hello nothing</span>
+        <div x-nothing="hello">hello, world!</div>
+        <div x-something="hello">hello, everyone!</div>
+        <span x-nothing="hello">hello nothing</span>
     </body>
-</html>`);
-        transformer.on('end', () => {
-            assert.equal(output , `<html>
+</html>`, `<html>
 <head>
     </head>
     <body>
-        <div X-nothing="censored">hello, world!</div>
-        <div X-something="hello">hello, everyone!</div>
-        <span X-nothing="hello">hello nothing</span>
+        <div x-nothing="censored">hello, world!</div>
+        <div x-something="hello">hello, everyone!</div>
+        <span x-nothing="hello">hello nothing</span>
     </body>
-</html>`);
-        });
+</html>`, done);
     }
 
-    @test attributeAsteriskTransformTest() {
-        const htmlBuffer = new stream.Duplex();
+    @test "Use * for tagname"(done: () => void) {
         const transformer = new Transformer();
         let output = "";
 
-        transformer.onTag("*", "X-nothing", /hello/, text => "censored");
-        htmlBuffer.pipe(transformer);
-        transformer.on('data', (d : any) => output = output + d);
-
-        transformer.setEncoding("utf-8");
-        transformer.end(`<html>
+        transformer.onTag("*", "x-nothing", /hello/, text => "censored");
+        transformTest(transformer, `<html>
 <head>
     </head>
     <body>
-        <div X-nothing="hello">hello, world!</div>
-        <div X-something="hello">hello, everyone!</div>
-        <span X-nothing="hello">hello nothing</span>
+        <div x-nothing="hello">hello, world!</div>
+        <div x-something="hello">hello, everyone!</div>
+        <span x-nothing="hello">hello nothing</span>
     </body>
-</html>`);
-        transformer.on('end', () => {
-            assert.equal(output , `<html>
+</html>`, `<html>
 <head>
     </head>
     <body>
-        <div X-nothing="censored">hello, world!</div>
-        <div X-something="hello">hello, everyone!</div>
-        <span X-nothing="censored">hello nothing</span>
+        <div x-nothing="censored">hello, world!</div>
+        <div x-something="hello">hello, everyone!</div>
+        <span x-nothing="censored">hello nothing</span>
     </body>
-</html>`);
-        });
+</html>`, done);
     }
 }
